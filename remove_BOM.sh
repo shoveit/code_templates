dff7619935d1312c2eb39543628d1acc66182191
Choulin <qiulin.work@gmail.com>
2015-09-09 17:15:05 +0800

    Update remove_BOM.sh

--- remove_BOM.sh
+++ remove_BOM.sh
@@ -1,2 +1,14 @@
+# 1.
 awk '{if(NR==1)sub(/^\xef\xbb\xbf/,"");print}' $1 > t
 mv t $1
+
+
+
+# 2. Gnu sed, Removing BOM from all text files in current directory:
+sed -i '1 s/^\xef\xbb\xbf//' *.txt
+
+
+# 3. FreeBSD/OS X sed,
+sed -i .bak '1 s/^\xef\xbb\xbf//' *.txt
+
+
